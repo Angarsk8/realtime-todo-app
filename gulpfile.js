8e18b0b79f22855595bc23ae79a9564192e5a8b5
Angarsk8 <andresa.garciah621@gmail.com>
2016-07-20 02:59:46 -0500

    manage front-end deps with npm and change structure of public folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,36 @@ const rename = require('gulp-rename')
 const babel  = require('gulp-babel')
 const minCSS = require('gulp-clean-css')
 
-const jsFiles = 'src/public/js/*.js'
+const jsFiles = 'src/public/js/src/*.js'
 const jsDest  = 'src/public/js/dist'
 
-const cssFiles = 'src/public/css/*.css'
+const cssFiles = 'src/public/css/src/*.css'
 const cssDest  = 'src/public/css/dist'
 
-gulp.task('build-js', () => {  
-	return gulp.src(jsFiles)
+// Front-end js libraries relative path
+const jquery         = 'node_modules/jquery/dist/jquery.js'
+const bootstrapjs    = 'node_modules/bootstrap/dist/js/bootstrap.js'
+const jqueryValidate = 'node_modules/jquery-validation/dist/jquery.validate.js'
+
+// Front-end css libraries relative path
+const bootstrapCss      = 'node_modules/bootstrap/dist/css/bootstrap.css'
+const bootstrapThemeCss = 'node_modules/bootstrap/dist/css/bootstrap-theme.css'
+const animateCss        = 'src/public/css/lib/animate.css'
+
+gulp.task('build-js', () => {
+	return gulp.src([jquery, bootstrapjs, jqueryValidate, jsFiles])
 		.pipe(concat('build.js'))
 		.pipe(babel({
 			presets: ['es2015']
 		}))
-		.pipe(gulp.dest(jsDest))
 		.pipe(rename('build.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest(jsDest))
 })
 
-
 gulp.task('minify-css', () => {
-  return gulp.src(cssFiles)
+  return gulp.src([bootstrapCss, bootstrapThemeCss, animateCss, cssFiles])
+    .pipe(concat('build.css'))
     .pipe(minCSS({compatibility: 'ie8'}))
     .pipe(rename('build.min.css'))
     .pipe(gulp.dest(cssDest))
